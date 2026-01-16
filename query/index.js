d5e047583d17c326fb1c9707df750e74aee76ae8
Kashoid23 <kashoyid@gmail.com>
2026-01-16 16:59:27 +0200

    Dealing with Missing Events

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] };
@@ -38,10 +33,29 @@ app.post('/events', (req, res) => {
             }
         }
     }
+};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
 app.listen(4002, () => {
     console.log('Listening on 4002');
+
+    axios.get('http://localhost:4005/events').then((res) => {
+        for (let event of res.data) {
+            console.log('Processing event:', event.type);
+            handleEvent(event.type, event.data);
+        }
+    }).catch((err) => {
+        console.log('Error fetching events from Event Bus', err.message);
+    });
 });
